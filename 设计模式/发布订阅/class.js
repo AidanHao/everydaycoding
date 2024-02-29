d8c0408412a7ce39adec2b302dd021d9e4cdd381
AidanHao <2072728972@qq.com>
2024-02-29 19:10:38 +0800

    '设计模式发布订阅'

---
+++ 设计模式/发布订阅/class.js
@@ -0,0 +1,59 @@
+// // 普及一下构造函数方法
+// function Point(x,y){
+//     this.x = x;
+//     this.y = y;
+// }
+// // 让p继承一些方法，在构造方法的原型上添加属性
+// // 这是ES5的写法 和java等面向对象语言差距比较大
+// Point.prototype.toString = function(){
+//     return `(${this.x},${this.y})`
+// }
+// // 特别的地方 foo不在构造函数的原型上，所有p继承不到foo 实例对象的隐式原型等于构造函数的显示原型
+// Point.foo = function(){
+//     return 'foo'
+// }
+// let p = new Point(1,2)
+
+
+// //在ES6增加了一个类的概念
+// class Point {
+//     // 类就是构造函数的变种 在类里面所有的this都指向这个类本身
+//     constructor(x,y){
+//         this.x = x;
+//         this.y = y;
+//     }
+//     // 加一个get关键字 可以直接p.toString 就可以当属性使用
+//     get toString(){
+//         return `(${this.x},${this.y})`
+//     }
+//     // 让实例对象访问不到foo 加一个static 静态方法
+//     static foo(){
+//         return 'foo'
+//     }
+// }
+
+// let p = new Point(1,2)
+
+// console.log(p.toString());
+
+
+
+
+// 新语法 可以省略constructor 但是要加变量名钱要加_
+// 例如ref
+class Point{
+    _count = 0
+    get value(){
+        // _可以不加，但是官方推荐加_
+        return this._count
+    }
+
+    set value(val){
+        console.log(val,'----');
+        this._count = val
+    }
+}
+let p = new Point()
+// console.log(p.value);
+// 赋值语句会触发set 读值读get
+p.value = 1
---
+++ 设计模式/发布订阅/emit.js
@@ -0,0 +1,87 @@
+// 手写一个发布订阅函数
+// 一般面试题长这样
+class EventEmitter {
+    constructor() {
+        // 用数组或者对象
+        this.event = {}// 'run':[fun]
+    }
+    // 用于订阅
+    on(type, cb) {
+        // 怎么知道emit掉没掉用呢？fn不能放在on里面执行
+        // 思路on负责接收回调函数，订阅事件，什么时候emit执行，这个回调函数就触发
+
+        // 如果读不到这个事件
+        if (!this.event[type]) {
+            this.event[type] = [cb];
+        } else {
+            // 如果曾经有人订阅过，我就把我的订阅事件加进去
+            this.event[type].push(cb);
+        }
+    }
+    // 订阅一次
+    once(type,cb) {
+        // 订阅一次就得取消
+        const fn = (...args) =>{
+            cb(...args)
+            this.off(type,fn)
+        }
+        this.on(type,fn)
+    }
+    //用于发布事件
+    emit(type, ...args) {
+        if (!this.event[type]) {
+            return
+        } else {
+            this.event[type].forEach(cb => {
+                // 打...把数组解构出来
+                cb(...args)
+            })
+        }
+    }
+    //用于取消订阅
+    off(type, cb) {
+        if (!this.event[type]) {
+            return
+        }else
+        {
+            // 使用filter过滤一下,排除了cb的对象
+            this.event[type] = this.event[type].filter(item=>item!==cb)
+        }
+    }
+}
+// 用法
+let ev = new EventEmitter();
+const fn = (a, b) => {
+    console.log(a, b, 1);
+}
+const fn1 = (a, b) => {
+    console.log(a, b, 2);
+}
+const fn2 = (a, b) => {
+    console.log(a, b, 'fn2');
+}
+const fn3 = (a, b) => {
+    console.log(a, b, 4);
+}
+// 订阅一个run事件
+// ev.on('run', fn)
+// 多个对象对同一个事件进行了订阅
+ev.on('run', fn1)
+
+ev.on('run', fn2)//只想订阅一次
+ev.once('run', fn2)
+
+ev.on('run', fn3)
+ev.off('run', fn3)
+
+
+// 发布一个run事件，还可以接收一个参数
+ev.emit('run', 1, 1)
+ev.emit('run', 2, 2)
+// ev.emit('run', 3, 3)
+
+
+
+// ev.on('say',fn)
+// ev.emit('say','hello world')
+
