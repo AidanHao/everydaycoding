a9a4dbf82d7a20db82c95a1f66411204bd6bf299
AidanHao <2072728972@qq.com>
2024-03-31 23:47:52 +0800

    'anycoding'

--- anycoding/1.js
+++ anycoding/1.js
@@ -1,4 +1,22 @@
-let a = 'sd'
-console.log(typeof a);
-let b = []
-console.log(b instanceof Array);
+// let a = 'sd'
+// console.log(typeof a);
+// let b = []
+// console.log(b instanceof Array);
+
+let mytry = function(){
+    return new Promise((resolve,reject)=>{
+        console.log(0);
+        resolve(1)
+        setTimeout(()=>{
+            resolve(2)
+        },1000)
+    })
+}
+mytry().then((res)=>{
+    console.log(res);
+    return new Promise((resolve,reject)=>{
+        resolve(3)
+    })
+}).then((res)=>{
+    console.log(res);
+})
