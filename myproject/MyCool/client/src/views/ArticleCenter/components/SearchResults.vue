81276c5efba5ae50b9c6f5c7dbfa5d94974301e5
AidanHao <2072728972@qq.com>
2025-05-23 20:56:03 +0800

    feat: 搜索时间

--- myproject/MyCool/client/src/views/ArticleCenter/components/SearchResults.vue
+++ myproject/MyCool/client/src/views/ArticleCenter/components/SearchResults.vue
@@ -50,7 +50,7 @@
                         <p class="article-summary">{{ article.articleDesc }}</p>
                         <div class="article-meta">
                             <span class="article-author">{{ article.authorName }}</span>
-                            <span class="article-date">{{ article.publishTime }}</span>
+                            <span class="article-date">{{ formatDate(article.publishTime) }}</span>
                             <span class="article-views">
                                 <el-icon><View /></el-icon>
                                 {{ article.viewCount }}
@@ -169,6 +169,14 @@ const defaultImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9
 const handleImageError = (e: Event) => {
     const img = e.target as HTMLImageElement;
     img.src = defaultImage;
+};
+
+const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 };
 </script>
 
